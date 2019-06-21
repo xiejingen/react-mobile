20cd14be1e55a0abbf4833d62801ad9544d61dad
xiejingen <15277472829>
2019-06-21 16:35:30 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './styles/App.css';
+import { Button, WhiteSpace} from 'antd-mobile';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <Button>default</Button><WhiteSpace />
+      <Button disabled>default disabled</Button><WhiteSpace />
+
+      <Button type="primary">primary</Button><WhiteSpace />
+      <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
+
+      <Button type="warning">warning</Button><WhiteSpace />
+      <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
+
+      <Button loading>loading button</Button><WhiteSpace />
+      <Button icon="check-circle-o">with icon</Button><WhiteSpace />
+      <Button icon={<img src="https://gw.alipayobjects.com/zos/rmsportal/jBfVSpDwPbitsABtDDlB.svg" alt="" />}>with custom icon</Button><WhiteSpace />
+      <Button icon="check-circle-o" inline size="small" style={{ marginRight: '4px' }}>with icon and inline</Button>
+      <Button icon="check-circle-o" inline size="small">with icon and inline</Button>
+   
     </div>
   );
 }
